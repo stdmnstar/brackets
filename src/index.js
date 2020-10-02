483bf2bb36005a098ebfe4d5356c83cbd4c9ec65
Aliaksandr Tarhonski <stdmnstar@gmail.com>
2020-10-03 01:48:45 +0300

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const twoBrackets = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        twoBrackets.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+
+    let oldStr = str;
+    while (true) {
+        for (let i = 0; i < twoBrackets.length; i++) {
+            str = str.replace(twoBrackets[i], '');
+        }
+
+        if (str.length == 0) {
+            return true;
+        }
+        if (oldStr === str) {
+            return false;
+        }
+        
+        oldStr = str;
+    }
+};
